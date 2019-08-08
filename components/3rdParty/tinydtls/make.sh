b2fa46177517df4b3e7cbb261101c42d04ad29f9
Simon Vogl <simon@voxel.at>
2019-08-08 13:23:17 +0200

    pass-thru target args for tinydtls

--- components/3rdParty/tinydtls/make.sh
+++ components/3rdParty/tinydtls/make.sh
@@ -10,12 +10,16 @@ elif [ x"$LE_CONFIG_CUSTOM_OS" = "xy" ]; then
     PLATFORM_CFLAGS="-I${LEGATO_ROOT}/3rdParty/Lwm2mCore/include/platform-specific/rtos -D__RTOS__"
 fi
 
+# unescape the TARGET_ARGS variable:
+ta=`eval echo $TARGET_ARGS`
+
 cp -rf ${LEGATO_ROOT}/3rdParty/Lwm2mCore/3rdParty/tinydtls/* .
 autoreconf -i
 ac_cv_func_malloc_0_nonnull=yes  ./configure \
     --prefix="${LEGATO_BUILD}/3rdParty/tinydtls" \
     --host="$($CC -dumpmachine)" \
-    CFLAGS="-w -std=gnu99 -DSIERRA -DWITH_SHA256 ${PLATFORM_CFLAGS} ${CFLAGS}" \
+    CFLAGS="-w $ta -std=gnu99 -DSIERRA -DWITH_SHA256 ${PLATFORM_CFLAGS} ${CFLAGS}" \
+    CPPFLAGS="-w $ta -std=gnu99 -DSIERRA -DWITH_SHA256 ${PLATFORM_CFLAGS} ${CFLAGS}" \
     LDFLAGS="${LDFLAGS}"
 make -k
 make -k install
